b5d06dd03303afb79a738b38932fdd436baf9cbf
lra <lra@r73.info>
2020-02-03 11:22:56 +0100

    rounded

--- src/CityDashboard.js
+++ src/CityDashboard.js
@@ -1,93 +1,81 @@
 import React from 'react';
 import './CityDashboard.css';
-import vodaImg from './voda.jpg'
 
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import { CardHeader } from '@material-ui/core';
-
-
-const useStyles = makeStyles({
-  container: {
-    backgroundColor: "#ccccaa",
-    padding: "5px",
-  },
-  card: {
-    height: "100%",
-    width: "100%",
-  },
-  cardWrapper: {
-    margin: '5px',
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  button: {
-    backgroundColor: 'yellow',
-    margin: '5px',
-    padding: '5px',
-  }
-});
-
+// svozovy plan: https://www.mestopacov.cz/assets/File.ashx?id_org=11721&id_dokumenty=2643
 
 
 function CityDashboard() {
-  const classes = useStyles();
 
-  const items = [];
-  items.push(
-    <Box className={classes.cardWrapper} key={'title'}>
-    <Card className={classes.card}>
-      <CardHeader title="Smart.Pacov.city" subheader="Přehled základních praktických údajů, které byste mohli potřebovat"/>
-      <CardContent>
-      </CardContent>
-    </Card>
-    </Box>
+  const content=[
+    {
+      gridLbl: 'title',
+      title:'Smart.Pacov.City',
+      subtitle:'praktické informace o městě Pacově přehledně',
+      text:['Údaje jsou platné pro aktuální rok 2020.']
+    },
+ 
+    {
+      gridLbl: 'element',
+      title:'Vodné/stočné',
+      subtitle:<span>cena za m<sup>3</sup></span>,
+      text:[
+        'Pacov, Roučkovice: 78,70 Kč',
+        'Velká Rovná, Zhoř, Bedřichov, Jetřichovec: 46,70 Kč',
+        'Hrádek, Malá Rovná: 34,70 Kč', 
+        'Salačova Lhota, Důl, Velká Chyška, Zhořec, Nová Ves, Bratřice: 24,98 Kč'
+      ]
+    },
+    {
+      gridLbl: 'element',
+      title:'Poplatek za odpad',
+      subtitle:'výše za rok',
+      text:[
+        'Trvale hlášená osoba: 700 Kč',
+        'Neobývaná nemovitost: 700 Kč',
+        'Splatnost: 30.6.'
+      ]
+    },
+    {
+      gridLbl: 'element',
+      title:'Poplatek ze psa',
+      subtitle:'výše za rok',
+      text:[
+        'první pes: 300 Kč (důch. 100Kč.)',
+        'druhý a další pes: 450 Kč (důch. 150 Kč)',
+        'první pes v bytovém domě: 700 Kč (důch. 200 Kč)',
+        'druhý a další pes v bytovém domě: 1050 Kč (důch. 300 Kč.)',
+        'první pes mimo Pacov: 60 Kč',
+        'druhý a další pes mimo Pacov: 90 Kč',
+        'Splatnost: 30.6.'
+      ]
+    },
+    {
+      gridLbl: 'element',
+      title:'Poslední uzavřené smlouvy',
+      subtitle:'přehled',
+      text:[
+        '21.2.2020, Pacovská lesní, 40 000 Kč',
+        '21.2.2020, Pacovská lesní, 40 000 Kč',
+        '21.2.2020, Pacovská lesní, 40 000 Kč',
+        '21.2.2020, Pacovská lesní, 40 000 Kč'
+      ]
+    }
+  ]
 
-  )
+  const items = content.map((v,idx)=>{
+    const text = v.text.map((t, txtIdx)=>(<p key={txtIdx}>{t}</p>))
+    return <div className={v.gridLbl} key={idx}>
+      <h1>{v.title}</h1>
+      <h2>{v.subtitle}</h2>
+      {text}
+    </div>
+  })
 
-  for (let i = 0; i < 20; i++) {
-    let grow=Math.random()>0.5?2:1
-    items.push((
-      <Box className={classes.cardWrapper} key={i}>
-        <Card  className={classes.card}>
-          <CardHeader title={`Vodné/stočné ${i+1}`} subheader="cena"/>
-          <CardContent>
-          Pacov: 97 Kč
-          </CardContent>
-        </Card>
-      </Box>
-    ))
-  }
+
 
   return (
-    <div className="CityDashboard">
-
-      <Box flexWrap="wrap" display="flex" 
-           flexDirection="row" className={classes.container} 
-           justifyContent="space-between" alignItems="stretch">
-        {items}
-      </Box>
-
-
+    <div className="container">
+      {items}
     </div>
   );
 }
